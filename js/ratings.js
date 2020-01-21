b0ac6d810a2b85861a496d8efd63e493a72d6a33
Hasan Tariq <htariq@reapit.com>
2020-01-21 14:42:31 +0000

    1-12 - Added Collect_Ratings function

---
+++ js/ratings.js
@@ -0,0 +1,18 @@
+function collect_ratings() {
+  const ratings { 'count': 0, 'sum': 0, 'average': 0 };
+
+  let rating = 0;
+
+  const elements = document.querySelectorAll('.rating');
+  elements.foreach(element => {
+    rating = parseInt(element.id.replace('star', ''));
+    ratings.count += parseInt(element.value);
+    ratings.sum += parseInt(element.value) * rating;
+  });
+
+  if (ratings.count != 0) {
+    ratings.average = ratings.sum / ratings.count;
+  }
+
+  return ratings;
+}
